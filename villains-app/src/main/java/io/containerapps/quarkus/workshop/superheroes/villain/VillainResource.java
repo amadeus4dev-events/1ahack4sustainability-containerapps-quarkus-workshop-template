248e104cb0b889caea15f58203057f2a40b5ccc4
Nicolas MEYER <nicolas.meyer@amadeus.com>
2022-09-27 09:54:20 +0200

    Adding /random villain endpoint

--- villains-app/src/main/java/io/containerapps/quarkus/workshop/superheroes/villain/VillainResource.java
+++ villains-app/src/main/java/io/containerapps/quarkus/workshop/superheroes/villain/VillainResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/api/villains")
 public class VillainResource {
@@ -13,5 +14,12 @@ public class VillainResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from the Villains App !\n";
+    }
+
+    @GET
+    @Path("/random")
+    public Response getRandomVillain() {
+        Villain villain = Villain.findRandom();
+        return Response.ok(villain).build();
     }
 }
