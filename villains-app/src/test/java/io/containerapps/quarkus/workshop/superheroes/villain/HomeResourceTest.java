073b42494739d93ff9a9f31934fa295e00d1ffac
tgrall <tugdual@gmail.com>
2022-10-02 11:30:14 +0200

    Add home resource for test

---
+++ villains-app/src/test/java/io/containerapps/quarkus/workshop/superheroes/villain/HomeResourceTest.java
@@ -0,0 +1,21 @@
+package io.containerapps.quarkus.workshop.superheroes.villain;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class HomeResourceTest {
+
+    @Test
+    public void testHomeEndpoint() {
+        given()
+          .when().get("/")
+          .then()
+             .statusCode(200)
+             .body(is( HomeResource.TEST_MSG   ));
+    }
+
+}
